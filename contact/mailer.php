74fb62322ff18975ec61275e438bb168eade22e1
Andrea <at.andreatai@gmail.com>
2019-05-31 18:25:59 -0700

    Updated Contact Form

--- contact/mailer.php
+++ contact/mailer.php
@@ -1,6 +1,6 @@
 <?php
      if(isset($_POST['submit'])) {
-         $myemail = 'at.andreatai@gmail.com';
+         $myemail = 'andrea@andreatai.com';
          $subject = $_POST['subject'];
          $name = $_POST['name'];
          $email = $_POST['email'];
@@ -8,6 +8,7 @@
          $headers = 'From: Contact Form <$myemail>\r\n';
          $headers .= 'Reply-To: $name <$email>\r\n';
          echo 'Your message has been sent successfully!';
+         echo '<a href="http://www.andreatai.com/contact">Return to Page</a>';
          mail($myemail, $subject, $message, $headers);
      } else {
          echo 'An error has occurred!';
