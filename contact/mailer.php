0111343a1ade4e2a20f208a6b1329327605964ec
Andrea <at.andreatai@gmail.com>
2019-05-31 18:28:57 -0700

    Updated success/error messages on Contact Form

--- contact/mailer.php
+++ contact/mailer.php
@@ -7,13 +7,9 @@
          $message = $_POST['message'];
          $headers = 'From: Contact Form <$myemail>\r\n';
          $headers .= 'Reply-To: $name <$email>\r\n';
-         echo 'Your message has been sent successfully!';
-         <br>
-         echo '<a href="http://www.andreatai.com/contact">Return to Page</a>';
+         echo 'Your message has been sent successfully! <a href="http://www.andreatai.com/contact">Return to Page</a>';
          mail($myemail, $subject, $message, $headers);
      } else {
-         echo 'An error has occurred!';
-         <br>
-         echo '<a href="http://www.andreatai.com/contact">Return to Page</a>';
+         echo 'An error has occurred! <a href="http://www.andreatai.com/contact">Return to Page</a>';
      }
  ?>
