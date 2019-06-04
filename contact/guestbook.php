a61c09843cf00bda97098ce240fb5c7b8b8ad99a
Andrea <at.andreatai@gmail.com>
2019-06-04 13:50:38 -0700

    updated guestbook

--- contact/guestbook.php
+++ contact/guestbook.php
@@ -1,24 +1,35 @@
-<h1>Guests</h1>
+<h1>Guestbook</h1>
 
-<table>
-  <?php
-  $con=mysqli_connect('pdb35.awardspace.net', '3009833_guestbook', 'andreataiguestbook32', '3009833_guestbook');
+<?php
+	if (!(empty($_POST["name"]) || empty($_POST["email"]) || empty($_POST["comment"])))
+	{
+		$name = $_POST["name"];
+		$email = $_POST["email"];
+		$comment = $_POST["comment"];
 
-  $sql="SELECT * FROM guestbook";
+		//insert_into
+		$con=mysqli_connect('pdb35.awardspace.net', '3009833_omis120database', 'omis120database', '3009833_omis120database');
+		$sql="INSERT INTO guestbook(name, email, comment)VALUES('$name', '$email', '$comment')";
+		$result=mysqli_query($con,$sql);
+		mysqli_close($con);
+	}
+	else
+	{
+		if (!empty($_POST["submitted"]))
+		{
+			$name_error = empty($_POST["name"]) ? '* name is required' : '';
+			$email_error = empty($_POST["email"]) ? '* email is required' : '';
+			$comment_error = empty($_POST["comment"]) ? '* comment is required' : '';
+		}
+	}
+?>
 
-  $result=mysqli_query($con, $sql);
-  if (mysqli_num_rows($result)>0) {
-    while($row = mysqli_fetch_array($result)) {
-      ?>
-      <tr>
-        <td> <?= $row["Name"] ?> </td>
-        <td> <?= $row["Email"] ?> </td>
-      </tr>
-      <?
-    }
-  } else{
-      print "There are no values";
-  }
-  mysqli_close();
-  ?>
-</table>
+<form action="guestbook.php" method="post">
+	Name: <input type="text" name="name"> <?= $name_error ?><br><br>
+	Email: <input type="email" name="email"> <?= $email_error ?><br><br>
+	Comment: <input type="text" name="comment"> <?= $comment_error ?><br><br>
+	<input type="hidden" name="submitted" value="true">
+	<input type="submit">
+</form>
+
+<a href="viewguestbook.php"> View Guestbook</a>
