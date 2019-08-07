aeb8a43b021e92031e5bacd1eda89bae61c6799c
Andrea <at.andreatai@gmail.com>
2019-08-07 14:13:09 -0700

    fix contact form

--- contact/mailer.php
+++ contact/mailer.php
@@ -1,18 +1,22 @@
-<?php
-     if(isset($_POST['submit'])) {
-         $myemail = 'andrea@andreatai.com';
-         $subject = $_POST['subject'];
-         $name = $_POST['name'];
-         $email = $_POST['email'];
-         $message = $_POST['message'];
-         $headers = 'From: Contact Form <$myemail>\r\n';
-         $headers .= 'Reply-To: $name <$email>\r\n';
-         mail($myemail, $subject, $message, $headers);
-         echo 'Your message has been sent successfully! Returning to previous page now...';
-     } else {
-         echo 'An error has occurred! Redirecting to contact form...';
-     }
+ <?php
 
-     header("Location: http://www.andreatai.com/contact"); /* Redirect browser */
-     exit();
- ?>
+    $myAwardSpaceEmail = "andrea@andreatai.com";
+    $myPersonalEmail = "at.andreatai@gmail.com";
+
+    if(isset($_POST['submit'])) {
+        $subject = $_POST['subject'];
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+        $headers = "From: Contact Form <" . $myAwardSpaceEmail . ">" . "\r\n";
+        $headers .= "Reply-To: " . $name . " <" . $email .">" . "\r\n";
+
+        echo 'Your message was sent successfully! Returning to previous page now...';
+        mail($myPersonalEmail, $subject, $message, $headers);
+    } else {
+        echo 'An error has occurred! Redirecting to contact form...';
+    }
+
+    header("Location: http://www.andreatai.com/contact");
+    exit();
+?>
