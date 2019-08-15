ebfa72c6cf009c29d6695c72633997147fa3aa44
Jeff Collins <htejeff@gmail.com>
2019-08-15 13:36:28 -0700

    Update mailer.php

--- contact/mailer.php
+++ contact/mailer.php
@@ -1,6 +1,6 @@
- <?php
+<?php
 
-    $myAwardSpaceEmail = "andrea@andreatai.com";
+    $myAwardSpaceEmail = "Contact Form <andrea@andreatai.com>";
     $myPersonalEmail = "at.andreatai@gmail.com";
 
     if(isset($_POST['submit'])) {
@@ -8,17 +8,22 @@
         $name = $_POST['name'];
         $email = $_POST['email'];
         $message = $_POST['message'];
-        $headers = "From: Contact Form - " . $name . " <" . $email . ">" . "\r\n";
+        $headers = "From: " . $name . " <" . $email . ">" . "\r\n";
 
+        mail($myAwardSpaceEmail, $subject, $message, $headers);
         echo 'Your message was sent successfully! Returning to previous page now...';
-        mail($myPersonalEmail, $subject, $message, $headers);
     } else {
-        echo 'An error has occurred! Redirecting to contact form...';
+        echo 'An error has occurred! Redirecting to contact form...
+        <script>
+          setTimeout(1500);
+          window.location.replace("https://andreatai.com/contact");
+        </script>';
     }
 
     echo '<script type="text/JavaScript">
-     window.location.replace("https://andreatai.com/");
-     </script>';
+               setTimeout(1500);
+               window.location.replace("https://andreatai.com/");
+          </script>';
     die();
     exit();
 ?>
