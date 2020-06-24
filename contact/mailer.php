873c6cfeedc48c9e1c3bf5676486124508ea8703
Andrea <41210141+saterday32@users.noreply.github.com>
2020-06-24 23:43:55 +0800

    update homepage, contact form, and about pages

--- contact/mailer.php
+++ contact/mailer.php
@@ -1,16 +1,17 @@
 <?php
 
-    $myAwardSpaceEmail = "Contact Form <andrea@andreatai.com>";
+
+    $myEmail = "Contact Form <andrea@andreatai.com>";
     $myPersonalEmail = "at.andreatai@gmail.com";
 
     if(isset($_POST['submit'])) {
         $subject = $_POST['subject'];
         $name = $_POST['name'];
         $email = $_POST['email'];
-        $message = $_POST['message'];
+        $message = $_POST['message'] . "\r\n\r\nkickdoor is the bestest in da westest.";
         $headers = "From: " . $name . " <" . $email . ">" . "\r\n";
 
-        mail($myAwardSpaceEmail, $subject, $message, $headers);
+        mail($myEmail, $subject, $message, $headers);
         echo 'Your message was sent successfully! Returning to previous page now...';
     } else {
         echo 'An error has occurred! Redirecting to contact form...
